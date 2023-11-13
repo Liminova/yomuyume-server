f1090b1e89829545f188b28769246fdc8553bb4d
j1nxie <rylieeeeexd@gmail.com>
2023-11-13 12:46:00 +0700

    fix(config): correct variable name

--- src/config.rs
+++ src/config.rs
@@ -12,7 +12,7 @@ impl Config {
     pub fn init() -> Self {
         let server_address = std::env::var("SERVER_ADDRESS").unwrap_or("127.0.0.1".to_string());
         let server_port = std::env::var("SERVER_PORT").unwrap_or(3000.to_string());
-        let database_url = std::env::var("DATABASE_URL").expect("SQLITE_URL must be set.");
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
         let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set.");
         let jwt_expires_in = std::env::var("JWT_EXPIRES_IN").expect("JWT_EXPIRES_IN must be set.");
         let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set.");
