7c7ea2f1d77c47d794ed638b2aafe9060a647d3f
j1nxie <rylieeeeexd@gmail.com>
2023-11-13 14:16:57 +0700

    fix(migrations): fix column restrictions

--- migrations/20231113053712_users.sql
+++ migrations/20231113053712_users.sql
@@ -2,8 +2,10 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id              TEXT PRIMARY KEY NOT NULL,
+    username        TEXT             NOT NULL,
     email           TEXT             NOT NULL,
-    profile_picture TEXT             NOT NULL,
-    created_at      DATETIME                 ,
-    updated_at      DATETIME                 ,
+    profile_picture TEXT                     ,
+    created_at      DATETIME         NOT NULL,
+    updated_at      DATETIME         NOT NULL,
+    password        TEXT             NOT NULL
 );
