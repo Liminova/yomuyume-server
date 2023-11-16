d5eceaef9410d0c01fd211a65a082ab42d6b4513
j1nxie <rylieeeeexd@gmail.com>
2023-11-16 13:33:57 +0700

    fix: remove `NOT NULL AUTOINCREMENT`
    sqlite already does this apparently

--- migrations/20231116040057_titles_tags.sql
+++ migrations/20231116040057_titles_tags.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS titles_tags
 (
-    id       INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-    title_id TEXT                NOT NULL              ,
-    tag_id   INTEGER             NOT NULL              ,
+    id       INTEGER PRIMARY KEY         ,
+    title_id TEXT                NOT NULL,
+    tag_id   INTEGER             NOT NULL,
     FOREIGN KEY(title_id) REFERENCES titles(id) ON DELETE CASCADE,
     FOREIGN KEY(tag_id)   REFERENCES tags(id)   ON DELETE CASCADE,
 );
