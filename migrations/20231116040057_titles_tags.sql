ebcc6551311df238271e145e716ba64ee0ad0768
j1nxie <rylieeeeexd@gmail.com>
2023-11-16 13:34:03 +0700

    fix: remove trailing `,`

--- migrations/20231116040057_titles_tags.sql
+++ migrations/20231116040057_titles_tags.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS titles_tags
     title_id TEXT                NOT NULL,
     tag_id   INTEGER             NOT NULL,
     FOREIGN KEY(title_id) REFERENCES titles(id) ON DELETE CASCADE,
-    FOREIGN KEY(tag_id)   REFERENCES tags(id)   ON DELETE CASCADE,
+    FOREIGN KEY(tag_id)   REFERENCES tags(id)   ON DELETE CASCADE
 );
