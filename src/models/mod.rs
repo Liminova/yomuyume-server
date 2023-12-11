dd653efccb536b9010997719aa2abb77c4855cdd
Delnegend <kiennguyen19323@gmail.com>
2023-12-11 20:42:01 +0700

    feat(models): :sparkles: add thumbnails table

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,6 +2,7 @@ pub mod auth;
 pub mod categories;
 pub mod pages;
 pub mod tags;
+pub mod thumbnails;
 pub mod titles;
 pub mod titles_tags;
 pub mod users;
--- src/models/prelude.rs
+++ src/models/prelude.rs
@@ -1,6 +1,7 @@
 pub use super::categories::Entity as Categories;
 pub use super::pages::Entity as Pages;
 pub use super::tags::Entity as Tags;
+pub use super::thumbnails::Entity as Thumbnails;
 pub use super::titles::Entity as Titles;
 pub use super::titles_tags::Entity as TitlesTags;
 pub use super::users::Entity as Users;
