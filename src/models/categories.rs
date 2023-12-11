7b20a8dc4a101175dc3f5f8f024750b76ebb2625
Delnegend <kiennguyen19323@gmail.com>
2023-12-11 20:43:22 +0700

    feat(models/categories): :sparkles: add thumbnail_id relation

--- src/models/categories.rs
+++ src/models/categories.rs
@@ -10,18 +10,26 @@ pub struct Model {
     pub id: String,
     pub name: String,
     pub description: Option<String>,
-    pub thumbnail: String,
+    pub thumbnail_id: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
     #[sea_orm(has_many = "super::titles::Entity")]
     Titles,
+    #[sea_orm(has_one = "super::thumbnails::Entity")]
+    Thumbnails,
 }
 
 impl Related<super::titles::Entity> for Entity {
     fn to() -> RelationDef {
         Relation::Titles.def()
+    }
+}
+
+impl Related<super::thumbnails::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Thumbnails.def()
     }
 }
 
