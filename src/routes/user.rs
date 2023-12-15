1dd4a645e59c6bf2c5910df2de4eb66aef406062
Delnegend <kiennguyen19323@gmail.com>
2023-12-15 18:08:10 +0700

    feat(routes/user): :sparkles: rm CheckReq struct; set pub CheckResBody

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,26 +1,21 @@
-use axum::{extract::Query, http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse};
 use serde::{Deserialize, Serialize};
-use utoipa::{IntoParams, ToSchema};
+use utoipa::ToSchema;
 
 use crate::utils::build_resp::build_resp;
 
 #[derive(Deserialize, Serialize, ToSchema)]
-struct CheckResponse {
+pub struct CheckResponseBody {
     success: bool,
-}
-
-#[derive(Deserialize, IntoParams, ToSchema)]
-pub struct CheckRequest {
-    pub echo: Option<String>,
 }
 
 #[utoipa::path(post, path = "/api/user/check", responses(
     (status = 200, description = "Cookies valid.", body = CheckResponse),
 ))]
-pub async fn get_check(_: Query<CheckRequest>) -> impl IntoResponse {
+pub async fn get_check() -> impl IntoResponse {
     build_resp(
         StatusCode::OK,
         String::from("Cookies valid."),
-        Some(CheckResponse { success: true }),
+        Some(CheckResponseBody { success: true }),
     )
 }
