03f4160b18b570843492e5185882a0dcc8dc3910
Delnegend <kiennguyen19323@gmail.com>
2023-12-18 18:02:59 +0700

    feat(main.rs): :sparkles: rewrite use; rename fn; add user routes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 use crate::middlewares::auth::auth;
-use crate::routes::auth::post_confirm::post_confirm;
-use crate::routes::auth::post_forget::post_forget;
 use crate::{config::Config, migrator::Migrator, routes::ApiDoc};
 use axum::{
-    middleware::from_fn_with_state,
+    middleware::from_fn_with_state as apply,
     routing::{get, post},
     Router,
 };
 use routes::{
-    auth::{get_logout::get_logout, post_login::post_login, post_register::post_register},
-    index::{get_categories::get_categories, get_title::get_title, post_filter::post_filter},
-    pages::*,
-    status::*,
-    user::*,
+    auth::{get_logout, post_login, post_register},
+    index::{get_categories, get_title, post_filter},
+    pages::{get_page, get_pages, get_pages_by_title_id},
+    status::{get_status, post_status},
+    user::{get_check, get_delete, get_reset, get_verify, post_delete, post_reset, post_verify},
 };
 use sea_orm::{ConnectionTrait, Database, DatabaseConnection, DbBackend, DbErr};
 use sea_orm_migration::prelude::*;
@@ -82,35 +80,34 @@ async fn main() -> Result<(), DbErr> {
         .route("/login", post(post_login))
         .route(
             "/logout",
-            get(get_logout).route_layer(from_fn_with_state(app_state.clone(), auth)),
-        )
-        .route("/forget", post(post_forget))
-        .route(
-            "/confirm",
-            post(post_confirm).route_layer(from_fn_with_state(app_state.clone(), auth)),
+            get(get_logout).route_layer(apply(app_state.clone(), auth)),
         );
 
     let user_routes = Router::new()
         .route("/check", get(get_check))
-        .layer(from_fn_with_state(app_state.clone(), auth));
+        .route("/reset", post(post_reset))
+        .route("/delete", get(get_delete).post(post_delete))
+        .route("/verify", get(get_verify).post(post_verify))
+        .layer(apply(app_state.clone(), auth));
 
     let index_routes = Router::new()
         .route("/filter", post(post_filter))
         .route("/categories", get(get_categories))
         .route("/title/:title_id", get(get_title))
-        .layer(from_fn_with_state(app_state.clone(), auth));
+        .layer(apply(app_state.clone(), auth));
 
     let pages_routes = Router::new()
         .route("/pages", get(get_pages))
         .route("/page/:page_id", get(get_page))
         .route("/pages/by_title_id/:title_id", get(get_pages_by_title_id))
-        .layer(from_fn_with_state(app_state.clone(), auth));
+        .layer(apply(app_state.clone(), auth));
 
     let app = Router::new()
         .nest("/api/auth", auth_routes)
         .nest("/api/index", index_routes)
         .nest("/api/pages", pages_routes)
         .nest("/api/user", user_routes)
+        .route("/api/user/reset", get(get_reset))
         .route("/api/status", get(get_status).post(post_status))
         .merge(SwaggerUi::new("/swagger").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
