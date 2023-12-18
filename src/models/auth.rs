bf61713c51a7434fea52c6138f65a38fa07ff575
Delnegend <kiennguyen19323@gmail.com>
2023-12-18 14:33:42 +0700

    feat(models/auth): :sparkles: jwt token now has purpose

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -1,23 +1,24 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+pub enum TokenClaimsPurpose {
+    VerifyRegister,
+    ResetPassword,
+    DeleteAccount,
+    None,
+}
+
+impl Default for TokenClaimsPurpose {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TokenClaims {
     pub sub: String,
     pub iat: usize,
     pub exp: usize,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub enum ConfirmReason {
-    Register,
-    Forget,
-    Delete,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct ConfirmTokenClaims {
-    pub sub: String,
-    pub iat: usize,
-    pub exp: usize,
-    pub reason: ConfirmReason,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub purpose: Option<TokenClaimsPurpose>,
 }
