f6aa593eaa6b10a4a83232cd2fdef1352a7571dd
Delnegend <kiennguyen19323@gmail.com>
2023-12-18 12:43:23 +0700

    fix(auth/mod.rs): :bug: post_forget utoipa struct

--- src/routes/auth/mod.rs
+++ src/routes/auth/mod.rs
@@ -4,5 +4,6 @@ pub mod post_login;
 pub mod post_register;
 
 pub use get_logout::__path_get_logout;
+pub use post_forget::__path_post_forget;
 pub use post_login::__path_post_login;
 pub use post_register::__path_post_register;
