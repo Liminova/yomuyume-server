a38672b8d235fc446b54815735397e36185338a6
Delnegend <kiennguyen19323@gmail.com>
2023-12-25 17:08:05 +0700

    feat(constants): :sparkles: native/extended img formats; thumbnail filestem

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,1 +1,15 @@
 pub mod version;
+
+pub fn thumbnail_filestem<'a>() -> Vec<&'a str> {
+    vec!["thumbnail", "cover", "_", "folder"]
+}
+
+pub fn extended_img_formats<'a>() -> Vec<&'a str> {
+    vec![
+        "jxl", "avif", "webp", "png", "jpg", "jpeg", "gif", "bmp", "tiff", "tif",
+    ]
+}
+
+pub fn native_img_formats<'a>() -> Vec<&'a str> {
+    vec!["png", "jpg", "jpeg", "gif", "bmp", "tiff", "tif"]
+}
