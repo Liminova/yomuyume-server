83979b92135fd88335486b453b0ed08e5c197d5d
Delnegend <kiennguyen19323@gmail.com>
2024-01-07 19:07:44 +0700

    refactor(thumbnail_finder): :recycle: unify to one function

--- src/livescan/thumbnail_finder.rs
+++ src/livescan/thumbnail_finder.rs
@@ -1,339 +1,182 @@
+use super::blurhash::{Blurhash, BlurhashResult};
+use crate::constants::{extended_img_formats, thumbnail_filestems};
+use rayon::prelude::*;
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+};
 use tracing::error;
 use zip::ZipArchive;
 
-use super::blurhash::{Blurhash, BlurhashResult};
-use std::{fs::File, path::PathBuf};
-
-/// Finds a valid thumbnail image from the category dir
+/// Find a valid thumbnail filename/path in a vector of filenames/paths
 ///
-/// This requires a Blurhash instance to try encode the thumbnail
-/// so that the returned image is actually a valid image
-///
-/// Only return the first match PathBuf to avoid wasteful iteration
-pub struct CategoryThumbnailFinder<'a> {
-    /// Where to look for the thumbnail image file
-    pub(super) parent_dir: &'a PathBuf,
+/// Doesn't check if the file actually exists or is a valid image
+struct ThumbnailPathFinder<'a> {
+    /// Just the filenames of the pages
+    pub(super) exist_filepaths: &'a Vec<String>,
     /// Explicit name of the file to look for
     /// (with or without extension both work)
     pub(super) explicit_name: &'a Option<String>,
-    /// List of implicit names of the file to look for
-    /// (without extension)
-    pub(super) implicit_names: &'a Vec<&'a str>,
-    /// File formats to look for
-    pub(super) formats: &'a Vec<String>,
-    /// An instance of Blurhash to encode the thumbnail
-    pub(super) blurhash: &'a Blurhash,
 }
 
-struct InternalCategoryResult {
-    path: PathBuf,
-    extension: String,
-}
-
-impl CategoryThumbnailFinder<'_> {
-    /// The first second one is the full path to the thumbnail to be store in DB
-    /// because no, I'm not adding another field to BlurhashResult
-    pub fn find(&mut self) -> Option<(BlurhashResult, PathBuf)> {
-        let result = self
+impl ThumbnailPathFinder<'_> {
+    pub fn find(exist_filepaths: &Vec<String>, explicit_name: &Option<String>) -> Option<String> {
+        let instance = ThumbnailPathFinder {
+            exist_filepaths,
+            explicit_name,
+        };
+        instance
             .both_explicit()
-            .or_else(|| self.explicit_name())
-            .or_else(|| self.both_implicit())
-            .or_else(|| self.fuzzy())?;
-        let blurhash_encoded = self.blurhash.encode(&result.path, &result.extension)?;
-        Some((blurhash_encoded, result.path))
-    }
-
-    /// Explicit name with explicit extension
-    fn both_explicit(&self) -> Option<InternalCategoryResult> {
-        let file = self.explicit_name.as_ref()?;
-        let parent_dir = self.parent_dir.to_path_buf();
-        let path: PathBuf = parent_dir.join(file);
-        if path.is_dir() {
-            return None;
-        }
-        let format = path
-            .extension()
-            .unwrap_or_default()
-            .to_string_lossy()
-            .to_string();
-        if self.formats.contains(&format) {
-            return Some(InternalCategoryResult {
-                path,
-                extension: format,
-            });
-        }
-        None
-    }
-
-    /// Explicit name with implicit extension
-    fn explicit_name(&self) -> Option<InternalCategoryResult> {
-        let file = self.explicit_name.as_ref()?;
-        let path = self.parent_dir.join(file);
-        if path.is_dir() {
-            return None;
-        }
-        self.formats.iter().find_map(|format| {
-            let mut path: PathBuf = path.clone();
-            path.set_extension(format);
-            path.is_file().then_some(InternalCategoryResult {
-                path,
-                extension: String::from(format),
-            });
-            None
-        })
-    }
-
-    /// Implicit name with implicit extension
-    fn both_implicit(&self) -> Option<InternalCategoryResult> {
-        self.implicit_names
-            .iter()
-            .flat_map(|name| {
-                self.formats
-                    .iter()
-                    .map(|format| {
-                        // let mut path = self.parent_dir.clone();
-                        // path.push(format!("{}.{}", name, format));
-                        // path
-                        let mut path = self.parent_dir.clone();
-                        path.push(name);
-                        path.set_extension(format);
-                        InternalCategoryResult {
-                            path,
-                            extension: format.clone(),
-                        }
-                    })
-                    .find(|result| result.path.is_file())
-            })
-            .next();
-        None
-    }
-
-    /// similar to full implicit search, but instead of explicitly creating
-    /// new <implicit_name>.<format> paths, we'll scan the whole parent_dir
-    /// to find the closest match to <implicit_name>
-    fn fuzzy(&self) -> Option<InternalCategoryResult> {
-        let paths: Vec<PathBuf> = self
-            .parent_dir
-            .read_dir()
-            .ok()?
-            .filter_map(|entry| entry.ok())
-            .filter(|entry| entry.path().is_file())
-            .filter(|entry| {
-                self.formats.iter().any(|format| {
-                    let file_extension = entry
-                        .path()
-                        .extension()
-                        .unwrap_or_default()
-                        .to_string_lossy()
-                        .to_string()
-                        .to_ascii_lowercase();
-                    let format = format.to_ascii_lowercase();
-                    file_extension == format
-                })
-            })
-            .map(|entry| entry.path())
-            .collect();
-
-        self.implicit_names
-            .iter()
-            .flat_map(|name| {
-                paths.iter().find(|path| {
-                    let file_stem = path
-                        .file_stem()
-                        .unwrap_or_default()
-                        .to_string_lossy()
-                        .to_string()
-                        .to_ascii_lowercase();
-                    file_stem.contains(&name.to_ascii_lowercase())
-                })
-            })
-            .next()
-            .map(|path| {
-                let extension = path
-                    .extension()
-                    .unwrap_or_default()
-                    .to_string_lossy()
-                    .to_string();
-                InternalCategoryResult {
-                    path: path.clone(),
-                    extension,
-                }
-            })
-    }
-}
-
-/// Find a valid thumbnail image from a Vec<BlurhashResult>
-///
-/// This guarantees that the returned image is actually a valid image
-///
-/// Only return the first match filename to avoid wasteful iteration
-pub struct TitleThumbnailFinder<'a> {
-    /// If a page exist in this vector, means it's actually a decode-able image
-    /// and not a file/folder with an image extension.
-    pub(super) blurhash_pages: &'a Vec<BlurhashResult>,
-    /// Explicit name of the file to look for
-    /// (with or without extension both work)
-    pub(super) explicit_name: &'a Option<String>,
-    /// List of implicit names of the file to look for
-    /// (without extension)
-    pub(super) implicit_names: &'a Vec<&'a str>,
-    /// File formats to look for
-    pub(super) formats: &'a Vec<String>,
-
-    /// So that I don't have to extract the filename list in every function
-    ///
-    /// Insert an empty vector here if I'm creating a new instance of this struct
-    pub(super) valid_pages: Vec<String>,
-}
-
-impl TitleThumbnailFinder<'_> {
-    /// This will always return something if the Vec<BlurhashResult> contains
-    /// something, I'm just wrapping it in an Option for consistency
-    pub fn find(&mut self) -> Option<BlurhashResult> {
-        self.valid_pages = self
-            .blurhash_pages
-            .iter()
-            .map(|page| page.file_name.clone())
-            .collect();
-
-        let found_page = self
-            .both_explicit()
-            .or_else(|| self.explicit_name())
-            .or_else(|| self.implicit())
-            .or_else(|| self.fuzzy());
-
-        if let Some(found_page) = found_page {
-            return self
-                .blurhash_pages
-                .iter()
-                .find(|page| page.file_name == found_page)
-                .cloned();
-        }
-        self.blurhash_pages.first().cloned()
+            .or_else(|| instance.explicit_name())
+            .or_else(|| instance.fuzzy())
     }
 
     fn both_explicit(&self) -> Option<String> {
-        let file_name = self.explicit_name.as_ref()?;
-        self.valid_pages
-            .contains(file_name)
-            .then(|| file_name.to_string())
+        let explicit_name = self.explicit_name.as_ref()?;
+        self.exist_filepaths
+            .iter()
+            .find(|path| path.to_ascii_lowercase().contains(explicit_name))
+            .map(|path| path.to_string())
     }
 
     fn explicit_name(&self) -> Option<String> {
-        let file_stem = self.explicit_name.as_ref()?;
-        self.formats.iter().find_map(|format| {
-            let mut path = PathBuf::from(file_stem);
-            path.set_extension(format);
-            let path = path.to_string_lossy().to_string();
-
-            self.valid_pages.contains(&path).then_some(path)
+        extended_img_formats().iter().find_map(|format| {
+            let mut filepath = PathBuf::from(self.explicit_name.as_ref()?);
+            filepath.set_extension(format);
+            let filename = filepath.to_string_lossy().to_string();
+            self.exist_filepaths
+                .iter()
+                .find(|path| path.to_ascii_lowercase().contains(&filename))
+                .map(|path| path.to_string())
         })
     }
 
-    fn implicit(&self) -> Option<String> {
-        self.implicit_names
-            .iter()
-            .flat_map(|name| {
-                self.formats
-                    .iter()
-                    .map(|format| format!("{}.{}", name, format))
-                    .find(|path| self.valid_pages.contains(path))
-            })
-            .next()
-    }
-
     fn fuzzy(&self) -> Option<String> {
-        self.implicit_names.iter().find_map(|name| {
-            self.valid_pages
+        thumbnail_filestems().iter().find_map(|possible_filestem| {
+            self.exist_filepaths
                 .iter()
                 .find(|path| {
-                    path.to_ascii_lowercase()
-                        .contains(&name.to_ascii_lowercase())
+                    let path = path.to_ascii_lowercase();
+                    let satisfy_filestem = path.contains(possible_filestem);
+                    let satisfy_extension = extended_img_formats()
+                        .iter()
+                        .any(|format| path.ends_with(format));
+                    satisfy_filestem && satisfy_extension
                 })
                 .map(|path| path.to_string())
         })
     }
 }
 
-/// Get called when the thumbnail field in <title>.toml changed
-pub struct TitleThumbnailChangeFinder<'a> {
-    pub(super) temp_dir: &'a PathBuf,
-    pub(super) title_path: &'a PathBuf,
-    pub(super) explicit_name: &'a Option<String>,
-    pub(super) implicit_names: &'a Vec<&'a str>,
-    pub(super) formats: &'a Vec<String>,
-    pub(super) blurhash: &'a Blurhash,
+/// Finds a valid thumbnail file in a directory, then encode it to blurhash,
+/// exhaustively, and return the first non-None result
+///
+/// ### Parameters
+/// - parent_dir: Where to look for the thumbnail image file
+/// - explicit_name: Explicit name of the file to look for (in <category>.toml)
+///  (with or without extension both work)
+/// - blurhash: An instance of Blurhash to encode the thumbnail
+///
+/// ### Returns a tuple
+/// - The first second one is the full path to the thumbnail to be store in DB
+/// because no, I'm not adding another field to BlurhashResult
+/// - The second one is the path to the thumbnail file
+pub fn thumbnail_finder(
+    parent_dir: &Path,
+    explicit_name: &Option<String>,
+    blurhash: &Blurhash,
+) -> Option<(BlurhashResult, PathBuf)> {
+    let filepaths = parent_dir
+        .read_dir()
+        .ok()?
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.path().is_file())
+        .filter(|entry| {
+            extended_img_formats().iter().any(|format| {
+                entry
+                    .path()
+                    .to_string_lossy()
+                    .to_ascii_lowercase()
+                    .ends_with(format)
+            })
+        })
+        .map(|entry| entry.path())
+        .collect::<Vec<PathBuf>>();
+
+    let filepaths_strs = filepaths
+        .iter()
+        .map(|entry| entry.to_string_lossy().to_string())
+        .collect::<Vec<String>>();
+
+    // Find a thumbnail file, blurhash-encode then return if Some(blurhash)
+    let result = ThumbnailPathFinder::find(&filepaths_strs, explicit_name);
+    if let Some(result) = result {
+        let result = PathBuf::from(result);
+        let extension = result
+            .extension()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_string();
+        let blurhash = blurhash.encode(&result, &extension);
+        if let Some(blurhash) = blurhash {
+            return Some((blurhash, result));
+        }
+    }
+
+    // Last resort, encode all if nothing found, return first non-None
+    filepaths
+        .par_iter()
+        .find_map_any(|path| {
+            let extension = path
+                .extension()
+                .unwrap_or_default()
+                .to_string_lossy()
+                .to_string();
+            blurhash.encode(path, &extension)
+        })
+        .map(|blurhash| (blurhash, filepaths[0].clone()))
 }
 
-impl TitleThumbnailChangeFinder<'_> {
-    pub async fn find(&mut self) -> Option<BlurhashResult> {
-        /* #region - read, extract zip file */
-        let title_temp_dir = {
-            let mut title_temp_dir = PathBuf::from(&self.temp_dir);
-            title_temp_dir.push(self.title_path.file_stem()?.to_string_lossy().to_string());
-            title_temp_dir
-        };
-        let title_file = File::open(self.title_path)
+/// Extract a title zip, then find a valid thumbnail file in it, then encode it
+/// to blurhash, exhaustively, and return the first non-None result
+pub async fn title_thumbnail_finder(
+    temp_dir: &PathBuf,
+    title_path: &PathBuf,
+    explicit_name: &Option<String>,
+    blurhash: &Blurhash,
+) -> Option<BlurhashResult> {
+    let title_temp_dir = {
+        let mut title_temp_dir = PathBuf::from(temp_dir);
+        title_temp_dir.push(title_path.file_stem()?.to_string_lossy().to_string());
+        title_temp_dir
+    };
+    ZipArchive::new(
+        File::open(title_path)
             .map_err(|e| {
                 error!("error openning title: {}", e);
                 e
             })
-            .ok()?;
-        ZipArchive::new(title_file)
-            .map_err(|e| {
-                error!("error reading title: {}", e);
-                e
-            })
-            .ok()?
-            .extract(&title_temp_dir)
-            .map_err(|e| {
-                let temp_dir = title_temp_dir.to_string_lossy();
-                error!("error extracting title to {}: {}", temp_dir, e);
-                e
-            })
-            .ok()?;
-        /* #endregion */
+            .ok()?,
+    )
+    .map_err(|e| {
+        error!("error reading title: {}", e);
+        e
+    })
+    .ok()?
+    .extract(&title_temp_dir)
+    .map_err(|e| {
+        let temp_dir = title_temp_dir.to_string_lossy();
+        error!("error extracting title to {}: {}", temp_dir, e);
+        e
+    })
+    .ok()?;
 
-        // Hear me out, Category thumbnail finder finds a thumbnail file given a
-        // parent directyory, which is excatly what we have here. Instead of
-        // re-encode the blurhash for every single pages, this only encodes the
-        // first file it founds.
-        CategoryThumbnailFinder {
-            parent_dir: &title_temp_dir,
-            explicit_name: self.explicit_name,
-            implicit_names: self.implicit_names,
-            formats: self.formats,
-            blurhash: self.blurhash,
-        }
-        .find()
-        // Return if found something
+    thumbnail_finder(&title_temp_dir, explicit_name, blurhash)
         .map(|(blurhash, _)| blurhash)
-        // Return the first page if nothing found
-        .or_else(|| {
-            let first_page = title_temp_dir
-                .read_dir()
-                .ok()?
-                .filter_map(|entry| entry.ok())
-                .filter(|entry| entry.path().is_file())
-                .filter(|entry| {
-                    self.formats.iter().any(|format| {
-                        let file_extension = entry
-                            .path()
-                            .extension()
-                            .unwrap_or_default()
-                            .to_string_lossy()
-                            .to_string()
-                            .to_ascii_lowercase();
-                        let format = format.to_ascii_lowercase();
-                        file_extension == format
-                    })
-                })
-                .map(|entry| entry.path())
-                .next()?;
-            self.blurhash.encode(
-                &first_page,
-                &first_page.extension().unwrap_or_default().to_string_lossy(),
-            )
+        .ok_or_else(|| {
+            let temp_dir = title_temp_dir.to_string_lossy();
+            error!("no thumbnail found in {}", temp_dir);
+            std::io::Error::new(std::io::ErrorKind::NotFound, "no thumbnail found")
         })
-    }
+        .ok()
 }
