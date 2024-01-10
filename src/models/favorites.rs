930fb32b38c45f7008b28da8cfd8da428216be82
Delnegend <kiennguyen19323@gmail.com>
2024-01-10 23:43:45 +0700

    fix(models/favorites): :bug: change type of id to number

--- src/models/favorites.rs
+++ src/models/favorites.rs
@@ -6,7 +6,7 @@ use utoipa::ToSchema;
 #[sea_orm(table_name = "favorites")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
-    pub id: String,
+    pub id: i32,
     pub user_id: String,
     pub title_id: String,
 }
