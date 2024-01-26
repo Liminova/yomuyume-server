baadc5ee03ea1c94b3b3ecd30edda0064fdcf48b
Delnegend <kiennguyen19323@gmail.com>
2024-01-26 23:46:35 +0700

    refactor(routes/file/mod): :recycle: replace with use *

--- src/routes/file/mod.rs
+++ src/routes/file/mod.rs
@@ -1,10 +1,5 @@
 mod get_page;
 mod thumbnail;
 
-pub use get_page::get_page;
-pub use thumbnail::get_thumbnail;
-pub use thumbnail::head_thumbnail;
-
-pub use get_page::__path_get_page;
-pub use thumbnail::__path_get_thumbnail;
-pub use thumbnail::__path_head_thumbnail;
+pub use get_page::*;
+pub use thumbnail::*;
