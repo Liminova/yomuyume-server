26a9972f72e97c7f8fb0557ed8eda6bc7eb7b900
Delnegend <kiennguyen19323@gmail.com>
2024-01-26 09:00:57 +0700

    refactor: :recycle: rename file to title_ssim_score

--- src/livescan/mod.rs
+++ src/livescan/mod.rs
@@ -4,13 +4,13 @@ mod handle_title;
 mod scan_category;
 mod scan_library;
 mod thumbnail_finder;
-mod title_relevance_score;
+mod title_ssim_score;
 
 use self::{
     blurhash::Blurhash,
     scan_library::{scan_library, ScannedCategory},
 };
-use crate::{livescan::title_relevance_score::title_ssim_score, models::prelude::*, AppState};
+use crate::{livescan::title_ssim_score::title_ssim_score, models::prelude::*, AppState};
 use sea_orm::{ColumnTrait, Condition, EntityTrait, QueryFilter};
 use std::{path::PathBuf, sync::Arc};
 
